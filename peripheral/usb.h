5b160835bd9087b20f7768198b4f64778b91498a
Lee Magnusson <lee@unhuman.io>
2019-03-22 14:02:15 -0700

    USB stale data cancel send working

--- peripheral/usb.h
+++ peripheral/usb.h
@@ -10,24 +10,27 @@ class USB {
     // limited to 64 bytes right now
     void send_data32(uint8_t endpoint, uint32_t *data, uint8_t length32) 
     {
+        GPIOC->ODR |= GPIO_ODR_OD13;
         //USBx_DEVICE->DIEPMSK &= ~USB_OTG_DIEPMSK_INEPNEM;
-
+    HAL_NVIC_DisableIRQ(OTG_FS_IRQn);
         if (USBx_INEP(endpoint)->DIEPCTL & USB_OTG_DIEPCTL_EPENA) {
             // TODO it would be nice to timeout/cancel old transmissions, instead just don't allow new transmissions
-            
-            // USBx_INEP(endpoint)->DIEPCTL |= USB_OTG_DIEPCTL_SNAK;
-            // while(!(USBx_INEP(endpoint)->DIEPINT & USB_OTG_DIEPINT_INEPNE));    //wait on nak
-            // USBx->GRSTCTL = ( USB_OTG_GRSTCTL_TXFFLSH |(uint32_t)( 1 << (USB_OTG_GRSTCTL_TXFNUM_Pos + endpoint)));
-            //  USBx_INEP(endpoint)->DIEPCTL |= USB_OTG_DIEPCTL_EPDIS;
-            // while(USBx->GRSTCTL & USB_OTG_GRSTCTL_TXFFLSH);     // wait on flush
-            // while(USBx_INEP(endpoint)->DIEPCTL & USB_OTG_DIEPCTL_EPENA);
-
-           return;
+           
+            USBx_INEP(endpoint)->DIEPCTL |= USB_OTG_DIEPCTL_SNAK;
+            while(!(USBx_INEP(endpoint)->DIEPINT & USB_OTG_DIEPINT_INEPNE));    //wait on nak
+            USBx->GRSTCTL = ( USB_OTG_GRSTCTL_TXFFLSH |(uint32_t)( 1 << (USB_OTG_GRSTCTL_TXFNUM_Pos + endpoint - 1)));
+            while(USBx->GRSTCTL & USB_OTG_GRSTCTL_TXFFLSH);     // wait on flush
+    //   //      USBx_INEP(endpoint)->DIEPCTL |= USB_OTG_DIEPCTL_EPDIS;
+      //      while(USBx_INEP(endpoint)->DIEPCTL & USB_OTG_DIEPCTL_EPENA);
+              
+//GPIOC->ODR &= ~GPIO_ODR_OD13;
+        //   return;
         }
 
 // __disable_irq();
-    HAL_NVIC_DisableIRQ(OTG_FS_IRQn);
 
+
+   
 
         USBx_INEP(endpoint)->DIEPTSIZ = 0;  // TODO necessary?
         USBx_INEP(endpoint)->DIEPTSIZ = sizeof(uint32_t) * length32 | (1 << USB_OTG_DIEPTSIZ_PKTCNT_Pos);
@@ -38,6 +41,7 @@ class USB {
      //   USBx_INEP(endpoint)->DIEPCTL |= USB_OTG_DIEPCTL_CNAK;  
     //     __enable_irq();
              HAL_NVIC_EnableIRQ(OTG_FS_IRQn);
+             GPIOC->ODR &= ~GPIO_ODR_OD13;
 
     }
 
