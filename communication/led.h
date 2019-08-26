8723302abd1fa10169c2d000b097294b50e4611c
Lee Magnusson <lee@unhuman.io>
2019-08-26 13:46:13 -0700

    Support rev0

--- communication/led.h
+++ communication/led.h
@@ -7,9 +7,10 @@
 // A tricolor led for status, pwm compare registers required
 class LED {
  public:
-    LED(uint16_t *const red_reg, uint16_t *const green_reg, uint16_t *const blue_reg, bool invert=false)
+    LED(uint16_t *const red_reg, uint16_t *const green_reg, uint16_t *const blue_reg, bool invert=false, float brightness=1.0)
         : red_reg_(red_reg), green_reg_(green_reg), blue_reg_(blue_reg) {
             invert_ = invert ? 1 : 0;
+            brightness_ = brightness;
         }
     enum Mode {OFF, ON, BLINKING, PULSING};
     enum Color {RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, WHITE};
@@ -23,14 +24,14 @@ class LED {
         }
         float t = i*(1.f/10000.f);
         float intensity = t < 0.5 ? 2*t : 2-2*t;
-        uint16_t intensity_period = 65535*fabsf(invert_ - intensity*intensity*intensity);
+        uint16_t intensity_period = 65535*brightness_*fabsf(invert_ - intensity*intensity*intensity);
         *red_reg_ = intensity_period;
         *green_reg_ = intensity_period;
         *blue_reg_ = intensity_period;
     }
  private:
     uint16_t i = 0;
-    float invert_;
+    float invert_, brightness_;
     uint16_t *const red_reg_;
     uint16_t *const green_reg_;
     uint16_t *const blue_reg_;
