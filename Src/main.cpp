5c79cb23b4d2524990d3822e1def5171dcdc5f31
Lee Magnusson <lee@unhuman.io>
2019-08-30 09:25:01 -0700

    Fixes for hsi trim code

--- Src/main.cpp
+++ Src/main.cpp
@@ -144,21 +144,30 @@ uint8_t calibrate_hsi(uint8_t hse_freq) {
     // hsi frequency is supposed to be 16 MHz
     // Switch to hsi for sysclk, connect hse_rtc to tim11 for counting
     RCC->APB2ENR |= RCC_APB2ENR_TIM11EN;
+    RCC->CFGR &= ~(RCC_CFGR_SW | RCC_CFGR_PPRE2);      // HSI at 16MHz for sysclk, APB2 at 16MHz for TIM11
+    RCC->CFGR |= (uint32_t) hse_freq << RCC_CFGR_RTCPRE_Pos; // HSE_RTC at 1 MHz
+
     TIM11->OR = 2;
     TIM11->CR1 |= TIM_CR1_CEN;
-    RCC->CFGR &= ~RCC_CFGR_SW;
-    int32_t min_error = (1<<31)-1;
+    TIM11->CCMR1 |= TIM_CCMR1_CC1S_0 | TIM_CCMR1_IC1PSC; // input capture IC1, max prescaler, 8
+    TIM11->CCER |= TIM_CCER_CC1E;     // enable input capture
+
+    int16_t min_error = INT16_MAX;
     uint8_t min_hsi_trim = 0x10;
     for (uint32_t i=0; i<=0x1F; i++) {
       uint32_t hsi_trim = i << RCC_CR_HSITRIM_Pos;
       RCC->CR &= ~RCC_CR_HSITRIM;
       RCC->CR |= hsi_trim;
-      uint32_t tstart = get_clock();
-      uint16_t thsestart = TIM11->CNT;
-      for (int j=0; j<1000; j++); // delay
-      uint32_t thsi = get_clock() - tstart;
-      uint32_t thse = TIM11->CNT - thsestart;
-      int32_t error = thse*16/24/2 - thsi;
+
+      uint16_t start_count = TIM11->CCR1;
+      uint16_t tmp[10];
+      for (int j=0; j<10; j++) {
+        while(!(TIM11->SR & TIM_SR_CC1IF)); // wait for capture
+        tmp[j] = TIM11->CCR1;
+      } 
+      // The ideal total count is 10*8*16
+      uint16_t total_count = TIM11->CCR1 - start_count;
+      int16_t error = 10*8*16 - (int16_t) total_count;
       if (abs(error) < abs(min_error)) {
         min_hsi_trim = i;
         min_error = error;
@@ -169,8 +178,9 @@ uint8_t calibrate_hsi(uint8_t hse_freq) {
 
 // Call bootloader, trigger is go_to_bootloader==1 on reboot + software reset
 uint8_t go_to_bootloader = false;
-uint32_t hsi_trim = 0;
+uint32_t hsi_trim = 0x10l << RCC_CR_HSITRIM_Pos;
 void reboot_to_bootloader() {
+  __disable_irq();
   go_to_bootloader = true;
   hsi_trim = (uint32_t) calibrate_hsi(get_pin_config()->crystal_frequency_MHz) << RCC_CR_HSITRIM_Pos;
   NVIC_SystemReset();
