61f1a5d8c6b7e412ea5f390c8022160609047310
Lee Magnusson <lee@unhuman.io>
2019-11-01 07:52:16 -0700

    Fix tim5 index pulse

--- Src/main.cpp
+++ Src/main.cpp
@@ -291,6 +291,7 @@ int main(void)
 			HAL_TIM_OC_Start(&htim8, TIM_CHANNEL_3);
 			HAL_TIM_OC_Start(&htim8, TIM_CHANNEL_4);
 			HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_3);
+      HAL_TIM_OC_Start(&htim5, TIM_CHANNEL_3);
       HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
       HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
       HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
