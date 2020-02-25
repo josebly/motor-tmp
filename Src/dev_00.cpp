3becabcc7982d86ee94a02210dc3a70288bb9980
Lee Magnusson <lee@unhuman.io>
2020-02-25 07:32:58 -0800

    Update dev_00 to current system

--- Src/dev_00.cpp
+++ Src/dev_00.cpp
@@ -1,12 +1,14 @@
 #include "../control/spi_encoder.h"
+#include "../control/qep_encoder.h"
 #include "../communication/usb_communication.h"
 #include "stm32f446xx.h"
 
 extern const volatile Param initial_param;
 
 static struct {
-    GPIO motor_encoder_cs = {*GPIOA, 15, GPIO::OUTPUT};
-    Encoder motor_encoder = {reinterpret_cast<volatile int32_t *>(&TIM2->CNT)};
+    GPIO cs = {*GPIOA, 15, GPIO::OUTPUT};
+    SPIEncoder output_encoder = {*SPI1, cs};
+    QEPEncoder motor_encoder = {reinterpret_cast<volatile int32_t *>(&TIM2->CNT), reinterpret_cast<volatile int32_t *>(&TIM2->CCR3)};
     GPIO enable = {*GPIOC, 14, GPIO::OUTPUT};
     PWM motor_pwm = {initial_param.fast_loop_param.pwm_frequency, *const_cast<uint32_t*>(&TIM8->CCR3), 
   //  SPIEncoder motor_encoder = {*SPI1, motor_encoder_cs};
@@ -17,7 +19,7 @@ static struct {
     LED led = {const_cast<uint16_t*>(reinterpret_cast<volatile uint16_t *>(&TIM3->CCR1)), 
                const_cast<uint16_t*>(reinterpret_cast<volatile uint16_t *>(&TIM3->CCR2)),
                const_cast<uint16_t*>(reinterpret_cast<volatile uint16_t *>(&TIM3->CCR4)), true};
-    PIDController controller;
+    PIDDeadbandController controller = {1.0/10000};
     USBCommunication communication;
-    MainLoop main_loop = {controller, communication, led};
+    MainLoop main_loop = {controller, communication, led, output_encoder};
 } config_items;
--- Src/util.h
+++ Src/util.h
@@ -7,5 +7,6 @@
 static inline uint32_t get_clock() { return DWT->CYCCNT; }
 static inline uint8_t get_cpi_count() { return DWT->CPICNT; }
 static inline uint8_t get_lsu_count() { return DWT->LSUCNT; }
+static inline void delay_ms(uint8_t ms) { uint32_t t_now = get_clock(); while((get_clock()-t_now) < ms*180e3); }
 
 #endif
