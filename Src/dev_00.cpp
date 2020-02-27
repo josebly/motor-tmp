f1d8b08c2c59b97a4fd1b111d2d251eab4c59e08
josebly <jlcordoba@fabulab.es>
2020-02-27 17:30:19 +0100

    Add files via upload

--- Src/dev_00.cpp
+++ Src/dev_00.cpp
@@ -1,14 +1,12 @@
 #include "../control/spi_encoder.h"
-#include "../control/qep_encoder.h"
 #include "../communication/usb_communication.h"
 #include "stm32f446xx.h"
 
 extern const volatile Param initial_param;
 
 static struct {
-    GPIO cs = {*GPIOA, 15, GPIO::OUTPUT};
-    SPIEncoder output_encoder = {*SPI1, cs};
-    QEPEncoder motor_encoder = {reinterpret_cast<volatile int32_t *>(&TIM2->CNT), reinterpret_cast<volatile int32_t *>(&TIM2->CCR3)};
+    GPIO motor_encoder_cs = {*GPIOA, 15, GPIO::OUTPUT};
+    Encoder motor_encoder = {reinterpret_cast<volatile int32_t *>(&TIM2->CNT)};
     GPIO enable = {*GPIOC, 14, GPIO::OUTPUT};
     PWM motor_pwm = {initial_param.fast_loop_param.pwm_frequency, *const_cast<uint32_t*>(&TIM8->CCR3), 
   //  SPIEncoder motor_encoder = {*SPI1, motor_encoder_cs};
@@ -19,7 +17,7 @@ static struct {
     LED led = {const_cast<uint16_t*>(reinterpret_cast<volatile uint16_t *>(&TIM3->CCR1)), 
                const_cast<uint16_t*>(reinterpret_cast<volatile uint16_t *>(&TIM3->CCR2)),
                const_cast<uint16_t*>(reinterpret_cast<volatile uint16_t *>(&TIM3->CCR4)), true};
-    PIDDeadbandController controller = {1.0/10000};
+    PIDController controller;
     USBCommunication communication;
-    MainLoop main_loop = {controller, communication, led, output_encoder};
+    MainLoop main_loop = {controller, communication, led};
 } config_items;
